61193dda03abe17b9ad7fb7279a510ce98907fb4
Denis Vilkov <denis_vilkov@epam.com>
2020-06-11 09:33:20 +0300

    fix: update ErrorStar layout

--- src/components/ErrorStar/ErrorStar.js
+++ src/components/ErrorStar/ErrorStar.js
@@ -4,7 +4,7 @@ import deathStarImg from '../../assets/img/DeathStar.png';
 
 function ErrorStar(props) {
   return (
-    <div className="error-star">
+    <div className="card error-star">
       <img className="error-star__img" src={deathStarImg} />
       <div>
         <h2>Error{props.errorMessage ? ':' : null}</h2>
